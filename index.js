272bab3ec08b7a553e0b168b8a412d0bf053411f
Noelle Foster <uufarmgirl@gmail.com>
2018-07-20 15:11:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 function updateObjectWithKeyAndValue(object, key, value){
-  
+
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  
+  object[key]=value
+  return object
 }
 
 function deleteFromObjectByKey(object, key){
-  
+
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
-  
+  delete object[key]
+  return object
 }
